221e25930832abd1d294eff70a5a0e8e7459855f
Juandvr <villabonajuan203@gmail.com>
2026-07-18 21:49:54 -0500

    pygame init

--- main.py
+++ main.py
@@ -1,5 +1,29 @@
+import pygame
+from game.settings import SCREEN_WIDTH, SCREEN_HEIGHT, FPS
+
 def main():
-    print("Hello from space-invaders!")
+    pygame.init()
+
+    gameDisplay = pygame.display.set_mode((SCREEN_WIDTH, SCREEN_HEIGHT))
+    pygame.display.set_caption('Space Invaders')
+
+    clock = pygame.time.Clock()
+
+    running = True
+
+    while running:
+
+        for event in pygame.event.get():
+            if event.type == pygame.QUIT:
+                running = False
+
+            print(event)
+
+        pygame.display.update()
+        clock.tick(FPS)
+
+    pygame.quit()
+    quit()
 
 
 if __name__ == "__main__":
